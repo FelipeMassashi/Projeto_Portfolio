7f0282a70ad84d7046708bd74b05c1183681e766
Felipe Massashi <Felipe Massashi>
2023-04-27 22:39:27 -0300

    js

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 
 const Earth = () => {
 
-  const earth = useGLTF('public/planet/scene.gltf')
+  const earth = useGLTF('./planet/scene.gltf')
 
   return (
     <primitive 
